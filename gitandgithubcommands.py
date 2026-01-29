46a6ba66fac1d79a11ca30031e31bb386b86dd28
masihaa-1 <vardhimasihaa@gmail.com>
2026-01-29 22:42:54 +0530

    initial command

---
+++ gitandgithubcommands.py
@@ -0,0 +1,8 @@
+from enum import auto
+
+
+>>>git config --global user.name"masihaa-1"
+>>>git config --global user.email"vardhimasihaa@gmail.com"
+>>>git config --global color.ui auto
+>>>git init
+get remote add origin https://github.com/masihaa-1/gitdemo.gitget remote add origin https://github.com/masihaa-1/gitdemo.git

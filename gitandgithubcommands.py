2b5d6679b03306de566703735a29a42ae2723ab6
masihaa-1 <vardhimasihaa@gmail.com>
2026-02-02 23:10:25 +0530

    Tutedude assignment submission

--- gitandgithubcommands.py
+++ gitandgithubcommands.py
@@ -5,4 +5,4 @@ from enum import auto
 >>>git config --global user.email"vardhimasihaa@gmail.com"
 >>>git config --global color.ui auto
 >>>git init
-get remote add origin https://github.com/masihaa-1/gitdemo.gitget remote add origin https://github.com/masihaa-1/gitdemo.git
+get remote add origin https://github.com/masihaa-1/gitdemo.gitget remote add origin https://github.com/masihaa-1/gitdemo.gitcd..

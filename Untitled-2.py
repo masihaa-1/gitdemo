10882f9e1f8858a4b7b8868745e25e75429a0688
masihaa-1 <vardhimasihaa@gmail.com>
2026-02-18 21:12:12 +0530

    Updated task3 with user input

---
+++ Untitled-2.py
@@ -0,0 +1,5 @@
+n=float(input("enter the number:"))
+if n>=0:
+    print("positive")
+else:
+    print("negative")
--- task2.2_loop.py
+++ task2.2_loop.py
@@ -1,4 +1,9 @@
+# Initialize sum variable
 total = 0
+
+# Loop from 1 to 50
 for i in range(1, 51):
- total += i
- print("The sum of numbers from 1 to 50 is:", total)
+    total += i #Add each number to total
+
+#Print final sum (outside the loop)
+print("The sum of numbers from 1 to 50 is:", total)
